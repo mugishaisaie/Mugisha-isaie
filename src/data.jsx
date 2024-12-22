98fd8553f8cc5ae4b65a5e9fae317cd50a771f32
mugisha isaie <mugishaisaieog1@gmail.com>
2024-12-22 19:54:48 +0200

    creating components folder and working on Navbar component

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,5 @@
 import { nanoid } from 'nanoid';
 import { FaHtml5, FaJs, FaReact } from 'react-icons/fa';
-
 export const links = [
   { id: nanoid(), href: '#home', text: 'home' },
   { id: nanoid(), href: '#about', text: 'about' },

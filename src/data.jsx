0498d08964af1f456042a330b486f1a12a9b7e65
mugisha isaie <mugishaisaieog1@gmail.com>
2024-12-31 07:05:16 +0200

    updating Skills component

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
-import { FaHtml5, FaJs, FaReact } from 'react-icons/fa';
+import { FaBootstrap, FaHtml5, FaJs, FaPhp, FaReact } from 'react-icons/fa';
+import { FaCss3 } from 'react-icons/fa6';
 export const links = [
   { id: nanoid(), href: '#home', text: 'home' },
   { id: nanoid(), href: '#skills', text: 'skills' },
@@ -11,22 +12,56 @@ export const links = [
 export const skills = [
   {
     id: nanoid(),
-    title: 'HTML&CSS',
+    title: 'HTML5',
     icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
-    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
+    text: 'Mastering the backbone of web development, I use HTML5 to create well-structured, semantic, and accessible web pages. With expertise in implementing modern features like audio, video, and responsive design elements, I ensure every web project is both user-friendly and visually engaging. HTML5 serves as the foundation of my web development journey.',
+  },
+  {
+    id: nanoid(),
+    title: 'CSS3 & Tailwind',
+    icon: <FaCss3 className='h-16 w-16 text-emerald-500' />,
+    text: `With CSS3 and Tailwind CSS Framework I'm Proficient in crafting responsive layouts, animations, and visually appealing designs , Expertise in Flexbox, Grid, and media queries to ensure cross-device compatibility. Skilled in optimizing styles for performance and maintainability and also Experienced in building modern, responsive and Amazingly UIs with Tailwind CSS. `,
   },
   {
     id: nanoid(),
     title: 'Javascript',
     icon: <FaJs className='h-16 w-16 text-emerald-500' />,
-    text: 'Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality',
+    text: `'I am proficient in JavaScript, the core language for interactive and dynamic web development. My expertise includes:
+ Mastery of ES6+ features such as arrow functions, destructuring, promises, and modules.
+DOM Manipulation: Building responsive, interactive user interfaces by dynamically updating and controlling HTML and CSS elements.
+APIs & Asynchronous Programming: Integrating RESTful APIs, handling data fetch requests, and ensuring smooth user experiences with async/await and event-driven programming.`,
   },
   {
     id: nanoid(),
-    title: 'React',
+    title: 'React JS',
     icon: <FaReact className='h-16 w-16 text-emerald-500' />,
-    text: 'Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.',
+    text: `As a frontend developer, I have hands-on experience with React.js, which I use to build dynamic and interactive web applications. With React, I am proficient in creating reusable components, managing state efficiently with React hooks (such as useState, useEffect, and useContext), and handling routing with React Router for seamless navigation.
+
+I am familiar with best practices like component-based architecture, functional components, and hooks. I have also worked with state management libraries like Redux and Context API to ensure smooth data flow across complex applications.`,
   },
+  {
+    id: nanoid(),
+    title: 'Bootstrap',
+    icon: <FaBootstrap className='h-16 w-16 text-emerald-500' />,
+    text: `I have hands-on experience using Bootstrap, a powerful and popular front-end framework for building responsive, mobile-first websites. With Bootstrap, I can quickly design and customize layouts, components, and utilities, ensuring that websites look great on all devices.
+With Bootstrap, I can quickly prototype and build fully responsive, visually appealing websites, boosting productivity and ensuring high-quality results.
+
+`,
+  },
+  {
+    id: nanoid(),
+    title: 'PHP and MYSQL ',
+    icon: <FaPhp className='h-16 w-16 text-emerald-500' />,
+    text: `
+    As a web developer with experience in PHP, I have developed a strong foundation in creating dynamic and interactive web applications. I am proficient in writing clean, maintainable, and efficient PHP code. My skills include:
+
+Server-Side Scripting: Creating powerful back-end logic to handle user requests, interact with databases, and generate dynamic content.
+Database Integration with in working with MySQL and other relational databases to create data-driven applications
+Form Handling & Data Validation and Performing Create,Read,Update and Delete Data using PHP code
+    `,
+  },
+  
+  
 ];
 
 export const projects = [
